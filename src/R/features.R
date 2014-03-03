77ba9de061268c3b08aaee4daa0d4ea16899dd6f
Nick Allen <nick@nickallen.org>
2014-03-03 10:33:49 -0500

    Created a separate source file to contain an "engineered features."

---
+++ src/R/features.R
@@ -0,0 +1,49 @@
+
+#
+# calculates the duration in days between a customer's first shopping 
+# contact and their last purchase contact. 
+#
+# WARNING: this makes a big assumption that the first contact to purchase 
+# is always within 7 days.  this is *probably* a fairly safe assumption 
+# for most of the data.  the data does not provide enough information
+# to do any better.
+#
+shopping.duration <- function (data) {
+  
+  # define the first and last shopping point for each record.  this makes
+  # the logic later more clear.  for example... 
+  #
+  # where shopping point = 1, the first is 1 and the last is 1.  
+  # where shopping point = 3, the first is 1 and the last is 3.
+  shopping.pts <- data [, list (
+    customer.id,
+    shopping.pt.first = 1,
+    shopping.pt.last = shopping.pt
+  )]
+  
+  # find the day of week for the first shopping point
+  setkeyv (data, c("customer.id", "shopping.pt"))
+  setkeyv (shopping.pts, c("customer.id", "shopping.pt.first"))
+  shopping.pts [ data, day.of.week.first := day.of.week]
+  
+  # find the day of week for the last shopping point
+  setkeyv (data, c("customer.id", "shopping.pt"))
+  setkeyv (shopping.pts, c("customer.id", "shopping.pt.last"))
+  shopping.pts [ data, day.of.week.last := day.of.week]
+  
+  # can assume that the point of first contact to purchase was within the same week
+  shopping.pts [ day.of.week.first <= day.of.week.last, 
+                 shopping.duration := day.of.week.last - day.of.week.first ]
+  
+  # must assume that the point of first contact was on week 1 and purchase on week 2
+  shopping.pts [ day.of.week.first >  day.of.week.last, 
+                 shopping.duration := day.of.week.last - (day.of.week.first - as.integer(7)) ]
+  
+  # merge the shopping duration back into the original feature/data set
+  setkey (data, "customer.id")
+  setkey (shopping.pts, "customer.id")
+  data [ shopping.pts, shopping.duration := shopping.duration]  
+  
+  # the input data is modified in-place
+  return (NULL)
+}
