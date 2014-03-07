2ca3d4883aeaeeb45693700d054ab53bd9eb0680
Nick Allen <nick@nickallen.org>
2014-03-07 15:22:15 -0500

    Simplified the way in which the data is flattened out for analysis.  This way is much quicker too.  Also, making some additional progress on modeling.  Its a bit messy right now.

--- src/R/shopping-models.R
+++ src/R/shopping-models.R
@@ -1,23 +1,4 @@
 
-#
-# models the shopping history and predicts a single option using 'gbm'. 
-#
-gbm.model <- function (option,
-                       train, 
-                       i.depth = c(1, 5, 9),
-                       n.trees = c(50, 100, 150, 200),
-                       shrink  = 0.1,
-                       control = trainControl (method = "cv", number = 5),
-                       ...) {
-  # tune and train the model
-  fit <- train (
-    method    = "gbm", 
-    trControl = control,
-    y         = as.factor (shopping [[option]]),
-    x         = shopping [, 2:23, with = F ],
-    tuneGrid  = expand.grid (interaction.depth = i.depth, n.trees = n.trees, shrinkage = shrink),
-    ... )
-}
 
 #
 # the popular model simply selects the most popular option in the training data.  
@@ -48,6 +29,10 @@ popular.model <- function (data) {
 }
 
 #
+# TODO - caret has some way to build our your own models using caret's infrastructure.  explore
+# that some more.
+#
+#
 # creates predictions from the "popular model" built using the 'popular.model' function.
 # this is an implementation of the generic function stats::predict.
 #
@@ -73,65 +58,38 @@ predict.popular.model <- function (model, newdata) {
 
 #
 # flattens a customer's shopping history into a single record that can then be 
-# used for training and prediction.
+# used for training and prediction.  the summarize function can be changed to 
+# summarize the shopping history in different ways.
 #
-flatten.shopping.history <- function (data) {
+extract.shopping.history <- function (data, summarize.f = sum) {
   
-  # determine how many times a customer shopped for each option choice
-  shopping <- data [ record.type == "shopping", list (
-    
-    # option a
-    option.a.0 = sum (option.a == 0),
-    option.a.1 = sum (option.a == 1),
-    option.a.2 = sum (option.a == 2),
-    
-    # option b
-    option.b.0 = sum (option.b == 0),
-    option.b.1 = sum (option.b == 1),
-    
-    # option c
-    option.c.1 = sum (option.c == 1),
-    option.c.2 = sum (option.c == 2),
-    option.c.3 = sum (option.c == 3),
-    option.c.4 = sum (option.c == 4),
-    
-    # option d
-    option.d.1 = sum (option.d == 1),
-    option.d.2 = sum (option.d == 2),
-    option.d.3 = sum (option.d == 3),
-    
-    # option e
-    option.e.0 = sum (option.e == 0),
-    option.e.1 = sum (option.e == 1),
-    
-    # option f 
-    option.f.0 = sum (option.f == 0),
-    option.f.1 = sum (option.f == 1),
-    option.f.2 = sum (option.f == 2),
-    option.f.3 = sum (option.f == 3),
-    
-    # option g
-    option.g.1 = sum (option.g == 1),
-    option.g.2 = sum (option.g == 2),
-    option.g.3 = sum (option.g == 3),
-    option.g.4 = sum (option.g == 4)
-    
-  ), by = customer.id ]
+  # extract only the shopping records
+  shopping <- data [record.type == "shopping"]
+  
+  # transform the options into dummy variables; option.a becomes (option.a0, option.a1, option.a2)
+  dummies <- dummyVars (~ option.a + option.b + option.c + option.d + option.e + option.f + option.g, shopping)
+  shopping <- data.table (customer.id = shopping$customer.id, predict (dummies, shopping))
+  
+  # add up the number of times a customer shopped for each option
+  shopping <- shopping [, lapply(.SD, summarize.f), by = customer.id ]
   setkey (shopping, "customer.id")
   
   # which options to the shopper actually purchase?
   purchase <- data [ record.type == "purchase", c("customer.id", options()), with = FALSE ]
   setkey (purchase, "customer.id")
   
+  # data table doesn't merge well with factors; the level names are lost
+  options.as.numeric (purchase)
+  
   # merge the shopping and purchase history
   shopping [ purchase, `:=` (option.a = option.a, 
-                             option.b = option.b,
-                             option.c = option.c,
-                             option.d = option.d,
-                             option.e = option.e,
-                             option.f = option.f,
-                             option.g = option.g )]
+                             option.b = option.b, 
+                             option.c = option.c, 
+                             option.d = option.d, 
+                             option.e = option.e, 
+                             option.f = option.f, 
+                             option.g = option.g) ]
+  options.as.factors (shopping)
   
   return (shopping)
 }
-
