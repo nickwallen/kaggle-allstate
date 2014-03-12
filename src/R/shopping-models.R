977c45b5c6a8c47dcf8a17246419fe0b41171dfa
Nick Allen <nick@nickallen.org>
2014-03-12 19:40:58 -0400

    Cleaned and reorganized some documentation.

--- src/R/shopping-models.R
+++ src/R/shopping-models.R
@@ -1,4 +1,53 @@
 
+#
+# trains, tunes, predicts, and creates a kaggle submission file for the current
+# champion shopping model.  ultimately the shopping model will need to be 
+# ensembled with the customer model.
+#
+champion.shopping.model <- function (verbose = FALSE) {
+  
+  # fetch the competition training data set and transform it for training
+  input <- fetch()
+  shopping.train <- extract.shopping.history (input, sum)
+  shopping.train <- extract.purchase.history (input, shopping.train)
+  
+  # defines how the parameter tuning will occur
+  control <- trainControl (method = "cv", number = 2)
+  
+  # which model parameters will be tuned?
+  tune.grid <- expand.grid (
+    n.trees           = c(50, 100, 200), 
+    shrinkage         = 0.1,
+    interaction.depth = c(1, 5, 9))
+  
+  # tune/train a separate model for each option
+  models <- lapply (options(), function (option) {
+    train (
+      method    = "gbm", 
+      trControl = control,
+      y         = shopping.train [[ option ]],
+      x         = shopping.train [, 2:23, with = F],
+      tuneGrid  = tune.grid,
+      verbose   = verbose )
+  })
+  
+  # name each of the models in the list
+  names(models) <- options.hat()
+  
+  # transform the test data for prediction
+  shopping.test <- extract.shopping.history (fetch (train = FALSE), sum)
+  
+  # each option has its own prediction model...
+  for (option.hat in names (models)) {
+    
+    # make a prediction for each option [a-g] using the correct model 
+    shopping.test [, option.hat := predict (models [[ option.hat ]], .SD), 
+                   .SDcols = 2:23, with = FALSE ]
+  }
+  
+  # create a submission file that can be uploaded to kaggle
+  create.submission (shopping.test)
+}
 
 #
 # the popular model simply selects the most popular option in the training data.  
@@ -29,9 +78,9 @@ popular.model <- function (data) {
 }
 
 #
-# TODO - caret has some way to build our your own models using caret's infrastructure.  explore
-# that some more.
-#
+# TODO - caret has some way to build your own models using caret's infrastructure.  explore
+# that some more.  building a separate predict implementation was just an experiement
+# so that it could be treated just like any other candidate model.
 #
 # creates predictions from the "popular model" built using the 'popular.model' function.
 # this is an implementation of the generic function stats::predict.
@@ -57,9 +106,17 @@ predict.popular.model <- function (model, newdata) {
 }
 
 #
-# flattens a customer's shopping history into a single record that can then be 
-# used for training and prediction.  the summarize function can be changed to 
-# summarize the shopping history in different ways.
+# a customer has one or more "shopping points" which characterize the options a 
+# customer shopped for in the past.  this function extracts and flattens a 
+# customer's shopping history into a single record that can then be used for 
+# training and prediction.  
+#
+# the 'summarize.func' argument defines how multiple shopping points are flattened 
+# into a single record for each customer.  the function can be changed to summarize 
+# the shopping history in different ways.  by default, the function simply sums the 
+# number of times a customer shopped for each option/selection {A0, A1, ..., B0, B1, ... }.
+#
+# the shopping history is available in both training and test sets.
 #
 extract.shopping.history <- function (data, summarize.func = sum) {
   
@@ -77,6 +134,14 @@ extract.shopping.history <- function (data, summarize.func = sum) {
   return (shopping)
 }
 
+#
+# there is always one, and only one, purchase record for each customer.  this 
+# record defines which options a customer actually purchased from AllState.  
+#
+# the competition is focused on predicting which options a customer will 
+# purchase.  of course, the purchase records are available only in training data, 
+# not the test data.
+#
 extract.purchase.history <- function (data, shopping) {
 
   # which options does the shopper actually purchase?
@@ -99,52 +164,4 @@ extract.purchase.history <- function (data, shopping) {
   options.as.factors (shopping)
   
   return (shopping)
-}
-
-#
-# runs the complete the shopping model and exports the results to a file
-# which can be submitted to kaggle.
-#
-champion.shopping.model <- function(verbose = FALSE) {
-  
-  # fetch the competition training data set and transform it for training
-  input <- fetch()
-  shopping.train <- extract.shopping.history (input, sum)
-  shopping.train <- extract.purchase.history (input, shopping.train)
-  
-  # defines how the parameter tuning will occur
-  control <- trainControl (method = "cv", number = 2)
-  
-  # which model parameters will be tuned?
-  tune.grid <- expand.grid (
-    n.trees           = c(50, 100, 200), 
-    shrinkage         = 0.1,
-    interaction.depth = c(1, 5, 9))
-  
-  # tune/train a separate model for each option
-  models <- lapply (options(), function (option) {
-    train (
-      method    = "gbm", 
-      trControl = control,
-      y         = shopping.train [[option]],
-      x         = shopping.train [, 2:23, with = F],
-      tuneGrid  = tune.grid,
-      verbose   = verbose )
-  })
-  
-  # name each of the models in the list
-  names(models) <- options.hat()
-  
-  # transform the test data for prediction
-  shopping.test <- extract.shopping.history (fetch (train = FALSE), sum)
-  
-  # make a prediction for each option
-  for (option.hat in names (models)) {
-    
-    shopping.test [, option.hat := predict (models[[option.hat]], .SD), 
-                   .SDcols = 2:23, with = FALSE ]
-  }
-  
-  # create a competition submission
-  create.submission (shopping.test)
 }
