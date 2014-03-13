36f28a270b0405b30ed1684eabff7c8cf8ffc4c0
Nick Allen <nick@nickallen.org>
2014-03-12 20:02:52 -0400

    Adding a way to source all files in the source directory to bootstrap things outside of simply using the markdown files in the analysis directory.  I guess converting this project into a package is the only way in R to do this right.

--- src/R/main.R
+++ src/R/main.R
@@ -1,1 +1,17 @@
 
+#
+# sources all R files in the current working directory.  i wish there was a 
+# better way to include all of the necessary files.
+#
+source.all <- function(path = getwd(), trace = TRUE, ...) {
+
+  # for each file...
+  for (file in list.files (path, pattern = "\\.[Rr]$")) {
+    if(trace) {
+      cat (sprintf ("sourcing %s...\n", file))
+    }
+
+    # source it!
+    source (file.path (path, file), ...)
+  }
+}
