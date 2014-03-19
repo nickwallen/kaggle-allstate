370f4d34936f72a957c8077e8a5630e49d3e755d
Nick Allen <nick@nickallen.org>
2014-03-19 16:46:13 -0400

    Renaming the 'champion' shopping model to 'alpha' since its not the champion and it has to have some kind of name.  Also mimiced the same structure for 'alpha' as a I did for the popular and naive models.

--- src/R/alpha-shopping-model.R
+++ src/R/alpha-shopping-model.R
@@ -1,5 +1,65 @@
 
 #
+# trains, tunes, predicts, and creates a kaggle submission file for the current
+# champion shopping model.  ultimately the shopping model will need to be 
+# ensembled with the customer model.  the summary.func argument is used 
+# the function used to flatten/summarize multiple shopping points
+#
+export.alpha.model <- function (summary.func = weighted.sum.most.recent, 
+                                verbose      = FALSE, 
+                                file         = "../../submissions/red-swingline-predictions-shopping.csv") {
+  
+  # fetch the competition training data set and transform it for training
+  data <- fetch ()
+  shopping.train <- extract.shopping.history (data, summary.func)
+  shopping.train <- extract.purchase.history (data, shopping.train)
+  
+  # train the alpha model
+  models <- train.alpha.model(shopping.train)
+  
+  # transform the test data for prediction
+  shopping.test <- extract.shopping.history (fetch (train = FALSE), summary.func)
+  
+  # each option has its own prediction model...
+  for (option.hat in names (models)) {
+    
+    # make a prediction for each option [a-g] using the correct model 
+    shopping.test [, option.hat := predict (models [[ option.hat ]], .SD), .SDcols = 2:23, with = FALSE ]
+  }
+  
+  # create a submission file that can be uploaded to kaggle
+  create.submission (shopping.test, file)
+}
+
+#
+# trains a separate model for each option.
+#
+train.alpha.model <- function (shopping.train) {
+  
+  # which model parameters will be tuned?
+  tune.grid <- expand.grid (
+    n.trees           = c(50, 100, 200), 
+    shrinkage         = 0.1,
+    interaction.depth = c(1, 5, 9))
+  
+  # tune/train a separate model for each option
+  models <- lapply (options(), function (option) {
+    train (
+      method    = "gbm", 
+      trControl = trainControl (method = "cv", number = 2),
+      y         = shopping.train [[ option ]],
+      x         = shopping.train [, 2:23, with = F],
+      tuneGrid  = tune.grid,
+      verbose   = verbose )
+  })
+  
+  # name each of the models in the list
+  names(models) <- options.hat()
+  
+  return (models)
+}
+
+#
 # a function which returns a weighted mean where the most recent
 # values (towards the end of the list) are given higher weights
 #
@@ -15,99 +75,6 @@ weighted.mean.most.recent <- function (x) {
 weighted.sum.most.recent <- function (x) {
   weights <- c(1:length(x)) / length(x)
   sum (x * weights)
-}
-
-#
-# trains, tunes, predicts, and creates a kaggle submission file for the current
-# champion shopping model.  ultimately the shopping model will need to be 
-# ensembled with the customer model.  the summary.func argument is used 
-# the function used to flatten/summarize multiple shopping points
-#
-champion.shopping.model <- function (summary.func = weighted.sum.most.recent, 
-                                     verbose      = FALSE, 
-                                     file         = "../../submissions/red-swingline-predictions-shopping.csv") {
-  
-  # fetch the competition training data set and transform it for training
-  input <- fetch()
-  shopping.train <- extract.shopping.history (input, summary.func)
-  shopping.train <- extract.purchase.history (input, shopping.train)
-  
-  # defines how the parameter tuning will occur
-  control <- trainControl (method = "cv", number = 2)
-  
-  # which model parameters will be tuned?
-  tune.grid <- expand.grid (
-    n.trees           = c(50, 100, 200), 
-    shrinkage         = 0.1,
-    interaction.depth = c(1, 5, 9))
-  
-  # tune/train a separate model for each option
-  models <- lapply (options(), function (option) {
-    train (
-      method    = "gbm", 
-      trControl = control,
-      y         = shopping.train [[ option ]],
-      x         = shopping.train [, 2:23, with = F],
-      tuneGrid  = tune.grid,
-      verbose   = verbose )
-  })
-  
-  # name each of the models in the list
-  names(models) <- options.hat()
-  
-  # transform the test data for prediction
-  shopping.test <- extract.shopping.history (fetch (train = FALSE), summary.func)
-  
-  # each option has its own prediction model...
-  for (option.hat in names (models)) {
-    
-    # make a prediction for each option [a-g] using the correct model 
-    shopping.test [, option.hat := predict (models [[ option.hat ]], .SD), 
-                   .SDcols = 2:23, with = FALSE ]
-  }
-  
-  # create a submission file that can be uploaded to kaggle
-  create.submission (shopping.test, file)
-}
-
-#
-# the popular model simply selects the most popular option that was shopped for 
-# in the training data.  popularity is determined by how many times customers 
-# have looked at a particular option in the shopping history.
-#
-popular.shopping.model <- function (file = "../../submissions/red-swingline-predictions-popular.csv") {
-  require (reshape2)
-  require (data.table)
-  
-  # the popular model requires no training - it simply chooses the most popular option
-  test <- fetch(train = FALSE)
-  
-  # find the total number of customers who chose each option
-  popular.options <- rbindlist ( list (
-    test [, list (option = "option.a", .N), by = list (choice = option.a)],
-    test [, list (option = "option.b", .N), by = option.b],
-    test [, list (option = "option.c", .N), by = option.c],
-    test [, list (option = "option.d", .N), by = option.d],
-    test [, list (option = "option.e", .N), by = option.e],
-    test [, list (option = "option.f", .N), by = option.f],
-    test [, list (option = "option.g", .N), by = option.g]
-  ))
-  
-  # find only the most popular choice for each option
-  popular.options <- popular.options[, .SD [ N == max (N) ], by = option]
-  popular.options <- t ( popular.options [, list(option, choice)]) [2, ]
-
-  # only the list of customers to predict for is needed from the test data
-  customers <- unique (test$customer.id)
-  
-  # predict the most popular options for every customer (TODO - this is ugly)
-  rows <- length (customers)
-  options.matrix <- matrix (rep (popular.options, rows), nrow = rows, byrow = TRUE)
-  predictions <- data.table (customer.id = customers, options.matrix)
-  setnames (predictions, paste0("V", 1:7), options.hat())  
-  
-  # create a submission file that can be uploaded to kaggle
-  create.submission (predictions, file)
 }
 
 #
@@ -136,7 +103,7 @@ extract.shopping.history <- function (data, summarize.func = sum, ...) {
   # summarize the options which were shopped for using summarize.func
   shopping <- shopping [, lapply(.SD, summarize.func, ...), by = customer.id ]
   setkey (shopping, "customer.id")
-
+  
   return (shopping)
 }
 
@@ -149,15 +116,17 @@ extract.shopping.history <- function (data, summarize.func = sum, ...) {
 # not the test data.
 #
 extract.purchase.history <- function (data, shopping) {
-
+  
   # which options does the shopper actually purchase?
   purchase <- data [ record.type == "purchase", c("customer.id", options()), with = FALSE ]
-  setkey (purchase, "customer.id")
+  if (nrow (purchase) == 0)
+    stop ("no purchase record(s) found.  these only exist in the training data.")
   
   # data table doesn't merge well with factors; the level names are lost
   options.as.numeric (purchase)
   
   # merge the shopping and purchase history
+  setkey (purchase, "customer.id")
   shopping [ purchase, `:=` (option.a = option.a, 
                              option.b = option.b, 
                              option.c = option.c, 
